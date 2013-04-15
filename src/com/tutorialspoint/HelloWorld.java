f4b2f422844c4612c25a15135e0ddde1b875bc0d
Damian Palpacelli <damian.palpacelli@gmail.com>
2013-04-15 20:18:39 -0300

    callbacks

--- src/com/tutorialspoint/HelloWorld.java
+++ src/com/tutorialspoint/HelloWorld.java
@@ -10,4 +10,12 @@ public class HelloWorld {
 	public void getMessage() {
 		System.out.println("Your Message : " + message);
 	}
+
+	public void init() {
+		System.out.println("Bean is going through init.");
+	}
+
+	public void destroy() {
+		System.out.println("Bean will destroy now.");
+	}
 }
--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -1,19 +1,16 @@
 package com.tutorialspoint;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
-   public static void main(String[] args) {
-      ApplicationContext context = 
-             new ClassPathXmlApplicationContext("Beans.xml");
+	public static void main(String[] args) {
 
-      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+				"Beans.xml");
 
-      objA.setMessage("I'm object A");
-      objA.getMessage();
-
-      HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
-      objB.getMessage();
-   }
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+		context.registerShutdownHook();
+	}
 }
